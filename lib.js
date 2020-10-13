b2951558e9f66d24270c4a68bfaec81c19f62ad5
月落 <gaozijian0411@foxmail.com>
2020-10-14 02:07:52 +0800

    合并不同时期更改，进一步类型化，准备添加通用类型模板进来

--- lib.js
+++ lib.js
@@ -1,5 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.mapToObj = exports.len = exports.keys = exports.set = exports.zipToDict = exports.dict = exports.list = exports.trustType = exports.assertType = exports.assert = exports.float = exports.str = exports.int = exports.insert = exports.max = exports.min = exports.sample = exports.extract = exports.byIdx = exports.sorted = exports.shuffle = exports.zip = exports.print = exports.all = exports.any = exports.enumerate = exports.range = exports.randint = exports.delay = void 0;
 async function delay(mis) {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -8,6 +12,11 @@ async function delay(mis) {
     });
 }
 exports.delay = delay;
+//不包括max 从0开始
+function randint(max) {
+    return Math.floor(Math.random() * max) % max;
+}
+exports.randint = randint;
 //仿python基础设施
 function* range(start, space, end) {
     //允许 range(a,c,b) range(b) range(a,b)
@@ -49,6 +58,7 @@ function print(...data) {
     console.log(...data);
 }
 exports.print = print;
+//将一个数组列表压缩为一个元组列表
 function* zip(...arraylikes) {
     let itors = arraylikes.map(v => v[Symbol.iterator]());
     for (;;) {
@@ -65,6 +75,143 @@ function* zip(...arraylikes) {
     }
 }
 exports.zip = zip;
+//两次zip还原
+let a = list(zip(...list(zip(...[[1, 2, 3], [2, 3, 4]]))));
+//基本操作
+//打乱
+//双向队列 支持中间删除
+const denque_1 = __importDefault(require("denque"));
+function shuffle(arl) {
+    //随机选择idx进行填空 建立idx表 填一个删一个
+    let dq = new denque_1.default();
+    let a = list(arl);
+    let idxs = list(range(len(a)));
+    idxs.forEach(v => dq.push(v));
+    //随机从dq中选择一个idx 然后把那个位置的idx删掉
+    let r = new Array(len(a));
+    for (let t of a) {
+        //t为新得到的单词
+        //选择一个idx
+        let i = randint(len(dq));
+        let idx = dq.get(i);
+        dq.removeOne(i);
+        //随机填空
+        r[idx] = t;
+    }
+    return r;
+}
+exports.shuffle = shuffle;
+//默认是升序
+//排序函数 未来的array实现将省略list 这个扫描过程
+function sorted(arl, key = null, sorttype = "ASC") {
+    let ret = list(arl).sort((a, b) => {
+        let [k, kk] = [-key(a), -key(b)];
+        return k - kk;
+    });
+    return ret;
+}
+exports.sorted = sorted;
+//按索引表 在iterable中选择值
+//通常基于迭代器的通用实现比较慢 基于可随机访问存储的数组实现比较快
+//未来将转换到多重签名实现上
+function byIdx(arl, idxs) {
+    let l = list(arl);
+    let ret = idxs.map(v => l[v]);
+    return ret;
+}
+exports.byIdx = byIdx;
+//不放回采样
+function extract(arl, count) {
+    //从一个列表中采样 不放回
+    let a = list(arl);
+    let idx = shuffle(range(len(a))).slice(0, count);
+    print(idx);
+    return byIdx(a, idx);
+}
+exports.extract = extract;
+//有放回采样
+function sample(arl, count) {
+    //从一个列表中采样 有放回
+    let a = list(arl);
+    let idx = list(range(len(a))).map(v => randint(len(a)));
+    return byIdx(a, idx);
+}
+exports.sample = sample;
+//数学
+exports.min = Math.min;
+exports.max = Math.max;
+/**
+ * 插入
+ * @param arl 数组
+ * @param point 插入位置 插入到这个位置的元素前面 为 0-len(arl) 的值
+ * @param val 插入值
+ */
+function insert(arl, point, val) {
+    let newar = [];
+    let a = list(arl);
+    a.forEach((v, idx) => {
+        if (point == idx)
+            newar.push(val);
+        newar.push(v);
+    });
+    if (len(a) == point)
+        newar.push(val);
+    return newar;
+}
+exports.insert = insert;
+function int(other) {
+    if (typeof other == "string")
+        return parseInt(other);
+    else if (typeof other == "number")
+        return other | 0;
+    else if ("toInt" in other) {
+        return other.toInt();
+    }
+    else
+        return 0;
+}
+exports.int = int;
+function str(n) {
+    if (assertType(n, "object")) {
+        return n.toString();
+    }
+    else if (assertType(n, "string"))
+        return n;
+    return new Number(n).toString();
+}
+exports.str = str;
+function float(other) {
+    if (typeof other == "string")
+        return parseFloat(other);
+    else if (typeof other == "number")
+        return other;
+    else if ("toFloat" in other) {
+        return other.toFloat();
+    }
+    else
+        return 0;
+}
+exports.float = float;
+//特殊工具函数
+function assert(n, msg) {
+    if (!n)
+        throw new Error(msg);
+}
+exports.assert = assert;
+function assertType(a, b) {
+    if (typeof b == "string")
+        return typeof a == b;
+    else if (typeof b == "function")
+        return a instanceof b;
+}
+exports.assertType = assertType;
+//!用于判断类型别名或接口，由于无法直接判断，这里直接“取信”即认为是如此 用于类型推导
+//此函数让编辑器相信某对象是某个类型
+//此函数对any使用时可实现自动类型提示转换 类似 as 关键字
+function trustType(o) {
+    return true;
+}
+exports.trustType = trustType;
 //数据容器构造区域
 function list(iter) {
     if (iter == null)
@@ -76,21 +223,16 @@ function list(iter) {
     return ret;
 }
 exports.list = list;
-function dict(iter) {
-    //构造一个map容器
-    let m = new Map();
-    for (let a of iter) {
-        m.set(a[0], a[1]);
-    }
-    return m;
+function dict(arl) {
+    return new Map(arl);
 }
 exports.dict = dict;
-function set(iter) {
-    let m = new Set();
-    for (let a of iter) {
-        m.add(a);
-    }
-    return m;
+function zipToDict(ks, vs) {
+    return dict(zip(ks, vs));
+}
+exports.zipToDict = zipToDict;
+function set(arl) {
+    return new Set(arl);
 }
 exports.set = set;
 //数据操作
@@ -132,12 +274,44 @@ function len(obj) {
     }
 }
 exports.len = len;
-//这里实际扩展
-Array.prototype["reshape"] = function () {
-    //flat并重新填充数组
-};
-//采用函数转换方法
-function array(arr) {
-    return arr;
+//融合对象 制造一个可用任何key访问的object
+function mapToObj(map = new Map()) {
+    //
+    let r = new Proxy({}, {
+        get(target, p, receiver) {
+            return map.get(p);
+        },
+        set(target, p, value, receive) {
+            map.set(p, value);
+            return true;
+        },
+        has(target, p) {
+            return map.has(p);
+        },
+        deleteProperty(target, p) {
+            return map.delete(p);
+        },
+        defineProperty(target, p, attributes) {
+            map.set(p, attributes.value);
+            return true;
+        },
+        enumerate(target) {
+            return list(map.keys());
+        },
+        ownKeys(target) {
+            return list(map.keys());
+        }
+        // apply (target, thisArg: any, argArray?: any): any
+        // {
+        // },
+        // construct (target, argArray: any, newTarget?: any): object
+        // {
+        // }
+    });
+    return r;
 }
-let a = array(["hello"]);
+exports.mapToObj = mapToObj;
+//标识普通数组
+//原始列表未list 普通数组即弱类型数组 普通数组有的函数强类型数组一样有
+//实际分开实现
+//
--- lib.ts
+++ lib.ts
@@ -8,9 +8,13 @@ export async function delay(mis){
     })   
 }
 
+//不包括max 从0开始
+export function randint(max: number) {
+    return Math.floor(Math.random() * max) % max;
+}
 
 //仿python基础设施
-export function *range(start:number,space?:number,end?:number){
+export function *range(start:number,space?:number,end?:number):Iterable<number>{
     //允许 range(a,c,b) range(b) range(a,b)
     if(space==null&&end==null){
         //1
@@ -28,7 +32,7 @@ export function *range(start:number,space?:number,end?:number){
     }
 }
 
-export function* enumerate(arraylike:Iterable<any>){
+export function* enumerate<T>(arraylike:Iterable<T>):Iterable<[number,T]>{
     let now=0;
     for(let a of arraylike){
         yield [now++,a]
@@ -49,7 +53,12 @@ export function all(arraylike:Iterable<any>)
 export function print<T extends any[]>(...data:T){
     console.log(...data);
 }
-export function *zip(...arraylikes:Iterable<any>[]){
+
+type MapToIteratable<T extends any[]>={[R in keyof T]:Iterable<T[R]>};
+//用作类型引用
+type ZipType<T extends any[]>=Iterable<T>;
+//将一个数组列表压缩为一个元组列表
+export function *zip<T extends any[]>(...arraylikes:MapToIteratable<T>):Generator<T,void,void>{
     let itors=arraylikes.map(v=>v[Symbol.iterator]());
     for(;;){
         //对所有itor取next 如果全部成功则yield 否则返回
@@ -60,14 +69,155 @@ export function *zip(...arraylikes:Iterable<any>[]){
             //返回
             return undefined;
         }
-        else yield ress.map(v=>v.value);
+        else yield ress.map(v=>v.value) as T;
     }
 }
 
+//两次zip还原
+let a=list(zip(...list(zip(...[[1,2,3],[2,3,4]]))))
+
+//基本操作
+//打乱
+//双向队列 支持中间删除
+import Denque from "denque"
+export function shuffle<T>(arl:Iterable<T>):T[]{
+    //随机选择idx进行填空 建立idx表 填一个删一个
+    let dq=new Denque<number>()
+    let a=list(arl);
+    let idxs=list(range(len(a)));
+    idxs.forEach(v=>dq.push(v));
+    //随机从dq中选择一个idx 然后把那个位置的idx删掉
+    let r=new Array<T>(len(a))
+    for(let t of a){
+        //t为新得到的单词
+        //选择一个idx
+        let i=randint(len(dq));
+        let idx=dq.get(i)
+        dq.removeOne(i);
+        //随机填空
+        r[idx]=t;
+    }
+    return r;
+}
+//默认是升序
+//排序函数 未来的array实现将省略list 这个扫描过程
+export function sorted<T>(arl:Iterable<T>,key:(v:T)=>number=null,sorttype:"ASC"|"DESC"="ASC"){
+    let ret=list(arl).sort((a,b)=>{
+        let [k,kk]=[-key(a),-key(b)]
+        return k-kk;
+    })
+    return ret;
+}
+//按索引表 在iterable中选择值
+//通常基于迭代器的通用实现比较慢 基于可随机访问存储的数组实现比较快
+//未来将转换到多重签名实现上
+export function byIdx<T>(arl:Iterable<T>,idxs:number[]){
+    let l=list(arl);
+    let ret=idxs.map(v=>l[v]);
+    return ret;
+}
+//不放回采样
+export function extract<T>(arl:Iterable<T>,count:number):T[]{
+    //从一个列表中采样 不放回
+    let a=list(arl);
+    let idx=shuffle(range(len(a))).slice(0,count);
+    print(idx);
+    return byIdx(a,idx);
+}
+//有放回采样
+export function sample<T>(arl:Iterable<T>,count:number):T[]{
+    //从一个列表中采样 有放回
+    let a=list(arl);
+    let idx=list(range(len(a))).map(v=>randint(len(a)));
+    return byIdx(a,idx);
+}
+//数学
+export let min=Math.min;
+export let max=Math.max;
+
+/**
+ * 插入
+ * @param arl 数组
+ * @param point 插入位置 插入到这个位置的元素前面 为 0-len(arl) 的值
+ * @param val 插入值
+ */
+export function insert<T>(arl:Iterable<T>,point:number,val:T):T[]{
+    let newar=[]
+    let a=list(arl);
+    a.forEach((v,idx)=>{
+        if(point==idx) newar.push(val);
+        newar.push(v);
+    });
+    if(len(a)==point) newar.push(val);
+    return newar;
+}
+
+
+//基本数据
+interface AsInt{
+    toInt():number;
+}
+interface AsFloat{
+    toFloat():number;
+}
+export function int(other:string|number|AsInt){
+    if(typeof other=="string") return parseInt(other);
+    else if(typeof other=="number") return other|0;
+    else if("toInt" in other){
+        return other.toInt()
+    }else return 0;
+}
+type AsString={toString:()=>string}|;
+export function str(n:AsString){
+    if(assertType(n,"object")){
+        return n.toString();
+    }else if(assertType(n,"string")) return n;
+
+    return new Number(n).toString();
+}
+export function float(other:string|number|AsFloat){
+    if(typeof other=="string") return parseFloat(other);
+    else if(typeof other=="number") return other;
+    else if("toFloat" in other){
+        return other.toFloat()
+    }else return 0;
+}
+//特殊工具函数
+export function assert(n:boolean,msg:string){
+    if(!n) throw new Error(msg);
+    
+}
+
+type  TypeList= RawTypeList| "object" | "function";
+type RawTypeList="string" | "number" | "bigint" | "boolean" | "symbol" | "undefined";
+type TypeMap<name extends TypeList>=name extends "string"? string:
+                    name extends "number"? number:
+                    name extends "bigint"? bigint:
+                    name extends "boolean"? boolean:
+                    name extends "symbol"? symbol:
+                    name extends "function"? Function:
+                    name extends "undefined"? undefined:
+                    name extends "object"? object:never;
+type ClassType=new(...args)=>any;
+//此为类型判断函数系列 可用于类型推导 
+//用法: assertType(a,b) b为string时 判断原生类型 b为class时判断实例类型
+//! 不可用于判断接口类型和类型别名type 
+export function assertType<T extends TypeList>(obj:any,typename:T):obj is TypeMap<T>;
+export function assertType<T extends object,S extends ClassType>(obj:T,cls:S):obj is InstanceType<S>;
+export function assertType(a,b){
+    if(typeof b=="string") return typeof a==b;
+    else if(typeof b=="function")  return a instanceof b;
+}
+//!用于判断类型别名或接口，由于无法直接判断，这里直接“取信”即认为是如此 用于类型推导
+//此函数让编辑器相信某对象是某个类型
+//此函数对any使用时可实现自动类型提示转换 类似 as 关键字
+export function trustType<T>(o:any):o is T{
+    return true;
+}
 
 //数据容器构造区域
 
-export function list(iter?:Iterable<any>){
+export function list<T>(iter?:Iterable<T>):Array<T>{
     if(iter==null) return list([]);
     let ret=[]
     for(let a of iter){
@@ -75,22 +225,18 @@ export function list(iter?:Iterable<any>){
     }
     return ret;
 }
-export function dict<T1,T2>(iter?:Iterable<[T1,T2]>):Map<T1,T2>{
-    //构造一个map容器
-    let m=new Map<T1,T2>();
-    for(let a of iter){
-        m.set(a[0],a[1]);
-    }
-    return m;
+export function dict<K,V>(arl:Iterable<[K,V]>){
+    return new Map<K,V>(arl);
 }
-export function set<T>(iter?:Iterable<T>):Set<T>
+export function zipToDict<K,V>(ks:Iterable<K>,vs:Iterable<V>){
+    return dict(zip(ks,vs));
+}
+export function set<T>(arl:Iterable<T>)
 {
-    let m=new Set<T>();
-    for(let a of iter){
-        m.add(a);
-    }
-    return m;
+    return new Set<T>(arl);
 }
+
+
 
 //数据操作
 export function *keys<T extends object=any,K=any,V=any>(obj:T|Map<K,V>)
@@ -128,7 +274,48 @@ export function len(obj:Iterable<any>|HasLength|object){
         return s;
     }
 }
+//融合对象 制造一个可用任何key访问的object
 
+export function mapToObj(map:Map<any,any>=new Map()){
+    //
+    let r=new Proxy({},{
+        get(target,p:any,receiver){
+            return map.get(p);
+        },
+        set(target,p:any,value,receive){
+            map.set(p,value);
+            return true;
+        },
+        has(target,p:any){
+            return map.has(p);
+        },
+        deleteProperty (target, p): boolean{
+            return map.delete(p);
+        },
+        defineProperty (target, p, attributes: PropertyDescriptor): boolean
+        {
+            map.set(p,attributes.value);
+            return true;
+        },
+        enumerate (target): any[]
+        {
+            return list(map.keys());
+        },
+        ownKeys (target): any[]
+        {
+            return list(map.keys());
+        }
+        // apply (target, thisArg: any, argArray?: any): any
+        // {
+
+        // },
+        // construct (target, argArray: any, newTarget?: any): object
+        // {
+
+        // }
+    })
+    return r;
+}
 //把对象也转化为一个iterable
 
 
@@ -142,14 +329,14 @@ export function len(obj:Iterable<any>|HasLength|object){
 //实现 ndarray 和 ndarray<type>标识普通数组 ndarray<uint8> ndarray<int32> 等标识类型数组
 
 //强类型支持部分
-type uint8=void;
-type uint16=void;
-type uint32=void;
-type int8=void;
-type int16=void;
-type int32=void;
-type float32=void;
-type float64=void;
+type uint8={};
+type uint16={};
+type uint32={};
+type int8={};
+type int16={};
+type int32={};
+type float32={};
+type float64={};
 
 type int=int32;
 type uint=uint32;
@@ -165,24 +352,23 @@ type ForceType=integerType|floatType;
 type list<T>=Array<T>;
 
 
-//biaoshi多维数组
-type ndarray<T=any>=T extends ForceType ? TypedNDArray<T>:NDArray<T>;
 
 
 
 
 
 //标识有类型数组
-type TypedNDArray<T>=T extends uint8? Uint8Array:
+type TypedArray<T>=T extends uint8? Uint8Array:
                     T extends uint16? Uint16Array:
                     T extends uint32? Uint32Array:
                     T extends int8? Int8Array:
                     T extends int16? Int16Array:
                     T extends int32? Int32Array:
                     T extends float32? Float32Array:
-                    T extends float64? Float64Array:never;
+                    T extends float64? Float64Array:
+                    T extends any? GenericTypedArray:never;
 
-type TypedArray=Uint8Array|
+type GenericTypedArray=Uint8Array|
                 Uint16Array|
                 Uint32Array|
                 Int8Array|
@@ -196,23 +382,3 @@ type TypedArray=Uint8Array|
 //原始列表未list 普通数组即弱类型数组 普通数组有的函数强类型数组一样有
 //实际分开实现
 //
-interface NDArray<T=any> extends Array<T>{
-    shape:list<number>;
-    ndim:number;
-    //基本函数
-    reshape():NDArray<T>;
-    transpose():NDArray<T>;
-}
-//这里实际扩展
-Array.prototype["reshape"]=function(this:any[]){
-    //flat并重新填充数组
-    
-}
-
-//采用函数转换方法
-function array<T>(arr:T[]){
-    return arr as unknown as  ndarray<T>;
-}
-
-let a=array(["hello"])
-
